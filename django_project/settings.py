5d132310e11cedad42665a602d4b044b2af9209c
William Vita <williamvita@hotmail.com>
2024-01-20 11:04:47 -0400

    api-added

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # locales
     'books.apps.BooksConfig',
+    'apis.apps.ApisConfig', 
+    # terceros 
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
